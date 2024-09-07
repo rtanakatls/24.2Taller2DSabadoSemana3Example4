05576e0b7b9efe238b7428fde85774e00bcfb7c3
Ricardo Tanaka <rtanaka@talento.tls.edu.pe>
2024-09-07 10:22:11 -0500

    Added enmy

---
+++ Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._2Taller2DSabadoSemana3RPG
+{
+    internal class Enemy
+    {
+        private string name;
+        private int life;
+        private int damage;
+
+        public Enemy(string name, int life, int damage)
+        {
+            this.name = name;
+            this.life = life;
+            this.damage = damage;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            life -= damage;
+        }
+
+        public int GetDamage()
+        {
+            return damage;
+        }
+
+        public string GetData()
+        {
+            return $"Enemy: {name}, HP: {life}, Daño: {damage}";
+        }
+
+        public bool IsAlive()
+        {
+            return life > 0;
+        }
+    }
+}
