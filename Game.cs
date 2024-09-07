4b88aa86c11605736ed15d795d4f9a01e1bcc1d3
Ricardo Tanaka <rtanaka@talento.tls.edu.pe>
2024-09-07 10:30:50 -0500

    Added create player

--- Game.cs
+++ Game.cs
@@ -8,10 +8,41 @@ namespace _24._2Taller2DSabadoSemana3RPG
 {
     internal class Game
     {
+        private Player player;
+        private List<Enemy> enemies;
+
+        
         public void Execute()
         {
-            Console.WriteLine("Prueba");
+            CreatePlayer();
         }
+
+        public void CreatePlayer()
+        {
+            bool continueFlag = true;
+            Console.WriteLine("Introduce el nombre del jugador");
+            string name = Console.ReadLine();
+            int life = 0;
+            int damage = 0;
+            while (continueFlag)
+            {
+                Console.WriteLine("Introduce la vida del jugador");
+                life = int.Parse(Console.ReadLine());
+                Console.WriteLine("Introduce el daño del jugador");
+                damage = int.Parse(Console.ReadLine());
+                if (life < 0 || damage < 0 || life + damage > 100)
+                {
+                    Console.WriteLine("Los datos son inválidos");
+                }
+                else
+                {
+                    continueFlag = false;
+                }
+            }
+            player=new Player(name,life,damage);
+            Console.WriteLine($"Se creó a: {player.GetData()}");
+        }
+
 
     }
 }
